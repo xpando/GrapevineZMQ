97cb5a1fef15822d1378f815560ca65408194e11
xpando <xpando@gmail.com>
2014-01-28 14:28:37 -0600

    Turn Grapevine.Server into a TopShelf Windows Service

--- Grapevine.Server/Program.cs
+++ Grapevine.Server/Program.cs
@@ -1,28 +1,106 @@
-﻿using System;
-using System.Text;
-using Grapevine.Core;
+﻿using System.Configuration;
+using NLog;
+using Topshelf;
 using ZeroMQ;
 using ZeroMQ.Devices;
 
 namespace Grapevine.Server
 {
+    class ForwarderService
+    {
+        static Logger _logger = LogManager.GetCurrentClassLogger();
+
+        ZmqContext _context;
+        ForwarderDevice _forwarder;
+
+        public ForwarderService(string frontend, string backend)
+        {
+            _logger.Info("Creating 0mq context.");
+            _context = ZmqContext.Create();
+            _logger.Info("done.");
+
+            _logger.Info("Creating 0mq forwarder device.");
+            _forwarder = new ForwarderDevice(_context, frontend, backend, DeviceMode.Threaded);
+            _logger.Info("done.");
+
+            _logger.Info("Subscribe all on frontend.");
+            _forwarder.FrontendSetup.SubscribeAll();
+            _logger.Info("done.");
+        }
+
+        public void Start()
+        {
+            _logger.Info("Starting forwarder device.");
+            _forwarder.Start();
+            _logger.Info("done.");
+        }
+
+        public void Pause()
+        {
+            _logger.Info("Stopping forwarder device.");
+            _forwarder.Stop();
+            _logger.Info("done.");
+        }
+
+        public void Stop()
+        {
+            _logger.Info("Disposing forwarder device.");
+            _forwarder.Dispose();
+            _logger.Info("done.");
+
+            _logger.Info("Disposing 0mq context.");
+            _context.Dispose();
+            _logger.Info("done.");
+        }
+    }
+
     class Program
     {
+        static string Frontend
+        {
+            get
+            {
+                var frontend = ConfigurationManager.AppSettings["frontend"];
+
+                if (string.IsNullOrEmpty(frontend))
+                    throw new ConfigurationErrorsException("Missing frontend appSetting");
+
+                return frontend;
+            }
+        }
+
+        static string Backend
+        {
+            get
+            {
+                var backend = ConfigurationManager.AppSettings["backend"];
+
+                if (string.IsNullOrEmpty(backend))
+                    throw new ConfigurationErrorsException("Missing backend appSetting");
+
+                return backend;
+            }
+        }
+
         static void Main(string[] args)
         {
-            // TODO: turn this into a TopShelf service.
-
-            using (var context = ZmqContext.Create())
-            using (var forwarder = new ForwarderDevice(context, "tcp://*:18901", "tcp://*:18902", ZeroMQ.Devices.DeviceMode.Threaded))
+            HostFactory.Run(x =>
             {
-                // forward all messages
-                forwarder.FrontendSetup.SubscribeAll();
+                x.Service<ForwarderService>(s =>
+                {
+                    s.ConstructUsing(name => new ForwarderService(Frontend,Backend));
+                    s.WhenStarted(svc => svc.Start());
+                    s.WhenPaused(svc => svc.Pause());
+                    s.WhenContinued(svc => svc.Start());
+                    s.WhenStopped(svc => svc.Stop());
+                });
 
-                Console.WriteLine("Starting server...");
-                forwarder.Start();
-                Console.WriteLine("Server started. Press any key to exit.");
-                Console.ReadKey();
-            }
+                x.StartAutomaticallyDelayed();
+                x.RunAsNetworkService();
+                x.SetDescription("Grapevine Message Forwarding Service");
+                x.SetDisplayName("GrapevineForwarder");
+                x.SetServiceName("GrapevineForwarder");
+            });
         }
     }
 }
